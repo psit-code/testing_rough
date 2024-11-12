4ca003363cf4108ea78a6fc371daba6f3dc15de2
psit-code <insallah.liv@outlook.com>
2024-11-12 12:22:08 +0530

    Add files via upload

---
+++ first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+   int i ;
+   printf("enter i value");
+   scanf("%d",&i);
+   if(i%2==0){
+     printf("it is even");
+   }
+   else{
+     printf("it is odd");
+   }
+   return 0;
+}
